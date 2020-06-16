bade8a8de85b72a0be7f7f6bfa095e0545f3391a
YnsEmreArpacii <49341703+YnsEmreArpacii@users.noreply.github.com>
2020-06-16 03:05:55 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -6,19 +6,6 @@ const ayarlar = require('../ayarlar.json');
 var prefix = ayarlar.prefix;
 
 module.exports = client => {
-var oyun = [
-        "🔥",
-        "✨",
-        "⭐️"  
-    ];
-
-    setInterval(function() {
-
-        var random = Math.floor(Math.random()*(oyun.length-0+1)+0);
-
-        client.user.setActivity(oyun[random], "https://www.instagram.com/yyarpacii" );
-        }, 2 * 2500);
-    
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
   client.user.setStatus("online");
